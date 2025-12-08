88925d59e84cb2e2865fde4ae99053d87dcc9792
jerememetan <129954878+jerememetan@users.noreply.github.com>
2025-12-08 21:01:42 +0800

    deployment updates

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/LetMeSignUpPls/', // Replace with your GitHub repo name
   plugins: [
     vue(),
     vueDevTools(),
